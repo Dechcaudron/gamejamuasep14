6f5ad491e3a61f6a699701e7a68a425db88e834e
Héctor Barreras Almarcha <hbaelx@gmail.com>
2014-09-20 14:41:38 +0200

    - Camera movement and basic level

---
+++ unityProject/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour
+{	
+		public GameObject Player;
+		public int PixelMarginsH;
+		public int PixelMarginsVUp;
+		public int PixelMarginsVDown;
+		public float MoveSpeed;
+
+		// Update is called once per frame
+		void Update ()
+		{
+				Vector3 screenPosition = camera.WorldToScreenPoint (Player.transform.position);
+
+				if (screenPosition.x > Screen.width - PixelMarginsH) {
+						transform.Translate (MoveSpeed * Time.deltaTime * (screenPosition.x - Screen.width + PixelMarginsH), 0, 0);
+				}
+
+				if (screenPosition.x < PixelMarginsH) {
+						transform.Translate (MoveSpeed * Time.deltaTime * (screenPosition.x - PixelMarginsH), 0, 0);
+				}
+
+				if (screenPosition.y > Screen.width - PixelMarginsVUp) {
+						transform.Translate (0, MoveSpeed * Time.deltaTime * (screenPosition.y - Screen.width + PixelMarginsVUp), 0);
+				}
+		
+				if (screenPosition.y < PixelMarginsVDown) {
+						transform.Translate (0, MoveSpeed * Time.deltaTime * (screenPosition.y - PixelMarginsVDown), 0);
+				}
+	 		
+		}
+}
