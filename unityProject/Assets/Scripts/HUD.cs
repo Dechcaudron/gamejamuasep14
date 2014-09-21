d3dfd215aae7b25094cfb239ba3bcff42f074437
Ignacio <gamesignacio@yahoo.es>
2014-09-21 17:57:50 +0200

    gui

--- unityProject/Assets/Scripts/HUD.cs
+++ unityProject/Assets/Scripts/HUD.cs
@@ -6,9 +6,13 @@ public class HUD : MonoBehaviour {
 	public Renderer life; 
 	public Renderer energy;
 
+	public TextMesh broadcast;
+	public TextMesh infoText;
 	// Use this for initialization
 	void Start () {
 		//life1 = life.GetComponent<Renderer> ();
+		//broadcast.text = "Ending\ntransmision\nclosing system...";
+		infoText.text = "";
 	}
 	
 	// Update is called once per frame
@@ -16,5 +20,11 @@ public class HUD : MonoBehaviour {
 		//lifeValue = GameController.Health;
 		life.material.SetTextureOffset ("_MainTex", new Vector2(0f,-GameController.Health * 0.5f));
 		energy.material.SetTextureOffset ("_MainTex", new Vector2(0,GameController.Energy * 0.5f));
+
+		if (GameController.Health == 0){
+			broadcast.text = "";
+			infoText.text = "Ending\ntransmision\nclosing system...";
+		}
+		
 	}
 }
